d6860d9da1cde9ae32ba39b20bc6803809d77782
Bruno Calixto <b.calixto28@gmail.com>
2019-03-09 18:50:30 -0300

    Inserindo FormGroup de Enderecos em uma lista

--- Demo-Start/src/app/customers/customer.component.ts
+++ Demo-Start/src/app/customers/customer.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators, AbstractControl, ValidatorFn } from '@angular/forms';
+import { FormGroup, FormBuilder, FormArray, Validators, AbstractControl, ValidatorFn } from '@angular/forms';
 
 import { debounceTime } from 'rxjs/operators';
 
@@ -39,6 +39,10 @@ export class CustomerComponent implements OnInit {
   customer: Customer = new Customer();
   emailMessage: string;
 
+  get addresses(): FormArray {
+    return <FormArray>this.customerForm.get('addresses');
+  }
+
   private validationMessages = {
     required: 'Please enter your email address.',
     email: 'Please enter a valid email address.'
@@ -58,7 +62,7 @@ export class CustomerComponent implements OnInit {
       notification: 'email',
       rating: [null, ratingRange(1, 5)],
       sendCatalog: true,
-      addresses: this.buildAddress(),
+      addresses: this.fb.array([this.buildAddress()]),
     });
 
     this.customerForm.get('notification').valueChanges.subscribe(
